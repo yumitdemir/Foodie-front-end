984a7fdbb7ff82320a7d7ad8e2d888f3a3ce43a0
Yumit Demir <yumitdemir@outlook.com>
2023-06-02 19:07:34 +0200

    Add title-black and paragraph-black new custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "title-black":"#262626",
+        "paragraph-black":"#262626",
+      }
+    },
   },
   plugins: [],
 }
