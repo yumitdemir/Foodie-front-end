6cb1808cab844d7b6f345ec287995e24df4be270
Yumit Demir <yumitdemir@outlook.com>
2023-06-02 17:37:07 +0200

    Finish the design of the search input field in the home page.

--- src/pages/Home/Components/SearchRecipie.jsx
+++ src/pages/Home/Components/SearchRecipie.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function SearchRecipie(props) {
     return (
-        <div className={"flex flex-col justify-center items-center "}>
+        <div className={"flex flex-col justify-center items-center mt-8"}>
             <div className={"flex-col flex w-full"}>
-                <p className={"text-3xl font-extrabold"}>Find your taste</p>
-                <input type="text" className={" w-full rounded-3xl px-5 py-8 text-2xl font-bold border-2 border-gray-800"}/>
+                <p className={"text-xl  md:text-4xl font-extrabold text-gray-900 ps-4"} style={{textShadow:"4px 4px 4px rgba(0,0,0,0.19)"}}>What can I make with <span className={"text-green-500"}>...</span></p>
+                <input placeholder={"Enter ingredients"} type="text" className={"shadow-md shadow-black w-full rounded-3xl p-5 text-2xl font-bold border-2 border-gray-800 :border-green-700"}/>
             </div>
 
         </div>
