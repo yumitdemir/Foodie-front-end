9fd9b14528cf3050c1018307af265ab1b6ccd3e4
Yumit Demir <yumitdemir@outlook.com>
2023-06-02 18:07:03 +0200

    Add search button next to the search input field.

--- src/pages/Home/Components/SearchRecipie.jsx
+++ src/pages/Home/Components/SearchRecipie.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import {BiSearchAlt} from "react-icons/bi";
 
 function SearchRecipie(props) {
     return (
         <div className={"flex flex-col justify-center items-center mt-8"}>
             <div className={"flex-col flex w-full"}>
-                <p className={"text-xl  md:text-4xl font-extrabold text-gray-900 ps-4"} style={{textShadow:"4px 4px 4px rgba(0,0,0,0.19)"}}>What can I make with <span className={"text-green-500"}>...</span></p>
-                <input placeholder={"Enter ingredients"} type="text" className={"shadow-md shadow-black w-full rounded-3xl p-5 text-2xl font-bold border-2 border-gray-800 :border-green-700"}/>
+                <p className={"text-xl  md:text-4xl font-extrabold text-gray-900 ps-4"}
+                   style={{}}>What can I make with <span
+                    className={"text-green-500"}>...</span></p>
+                <div className={"flex flex-row shadow-lg shadow-green-100 rounded-3xl border-2  border-green-600"}>
+                    <input placeholder={"Enter ingredients e.g. garlic rice onion"} type="text"
+                           className={"focus:outline-none border-e-0 rounded-e-none w-full rounded-3xl p-5 text-xl font-bold "}/>
+                    <button className={"rounded-e-2xl bg-green-600 flex items-center  px-8 font-bold text-white"}><BiSearchAlt className={"text-3xl"}/></button>
+                </div>
             </div>
+
 
         </div>
     );
