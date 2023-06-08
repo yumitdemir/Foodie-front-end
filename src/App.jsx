4303814527a0dcb4d4014ba634a60b4efa069306
Yumit Demir <yumitdemir@outlook.com>
2023-06-08 10:37:30 +0200

    Create the SearchList route in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Nav from "./components/nav/nav.jsx";
-import Index from "./pages/Home/Index.jsx";
+import Home from "./pages/Home/Home.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import SearchList from "./pages/SearchList/SearchList.jsx";
 
 function App(props) {
     const router = createBrowserRouter([
         {path: "/", element: <Navigate to="/Home" replace/>},
-        {path: "/Home", element: <Index/> },
+        {path: "/Home", element: <Home/> },
+        {path: "/search/:pantry/:order/:input", element: <SearchList/> },
     ])
     return (
         <>
