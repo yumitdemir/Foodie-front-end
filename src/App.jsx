03bc4721d2d75ba47c8c0e5df41ee155c8b98342
Yumit Demir <yumitdemir@outlook.com>
2023-06-08 20:21:45 +0200

    Change the way the routes are rendered.

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import Nav from "./components/nav/nav.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import {Routes, Route, createBrowserRouter, Navigate, RouterProvider, BrowserRouter, Router} from "react-router-dom";
 import SearchList from "./pages/SearchList/SearchList.jsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 function App(props) {
-    const router = createBrowserRouter([
-        {path: "/", element: <Navigate to="/Home" replace/>},
-        {path: "/Home", element: <Home/> },
-        {path: "/search/:pantry/:order/:input", element: <SearchList/> },
-    ])
+
 
     const queryClient = new QueryClient();
     return (
-        <QueryClientProvider  client={queryClient}>
-                <Nav/>
-                <RouterProvider router={router}/>
-                <Footer/>
-        </QueryClientProvider>
+
+
+        <BrowserRouter>
+            <Nav />
+            <Routes>
+                <Route path="/" element={<Navigate to="/Home" replace />} />
+                <Route path="/Home" element={<Home />} />
+                <Route path="/search/:pantry/:order/:input" element={<SearchList />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+
+
     );
 }
 
