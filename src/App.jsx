4f8bad2d7160fcd4d5c586b5eca55cd4acee78c8
Yumit Demir <yumitdemir@outlook.com>
2023-06-08 13:01:00 +0200

    Add react query for the searching recipes.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
 import SearchList from "./pages/SearchList/SearchList.jsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 function App(props) {
     const router = createBrowserRouter([
@@ -11,12 +12,14 @@ function App(props) {
         {path: "/Home", element: <Home/> },
         {path: "/search/:pantry/:order/:input", element: <SearchList/> },
     ])
+
+    const queryClient = new QueryClient();
     return (
-        <>
+        <QueryClientProvider  client={queryClient}>
                 <Nav/>
                 <RouterProvider router={router}/>
                 <Footer/>
-        </>
+        </QueryClientProvider>
     );
 }
 
--- src/pages/SearchList/SearchList.jsx
+++ src/pages/SearchList/SearchList.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
+import {useQuery} from "@tanstack/react-query";
+
+const fetchRecipes = (pantry, order, input) => {
+    const apiKey = '49a32b520f314d1294660ea61e7ff18e';
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${""}&number=${""}&apiKey=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .catch(error => console.error(error));
+};
 
 function SearchList(props) {
+    const {pantry, order, input} = useParams();
+
+    const recipes = useQuery(
+        [`recipies`],
+        () => fetchRecipes(pantry, order, input)
+    )
+
     return (
-        <div>test</div>
+        <div>{pantry + order + input}
+
+        </div>
     );
 }
 
