abc3fc7468b2843ea4ab401a716a5a42bc7839f2
Yumit Demir <yumitdemir@outlook.com>
2023-06-08 20:38:18 +0200

    Make the logo a Link that sends the user to the home page. Make options in navbar colored based on the current link root

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -3,21 +3,24 @@ import React from 'react';
 import {FaBars} from 'react-icons/fa';
 import {Container} from "postcss";
 import PageContainer from "../Container/PageContainer.jsx";
+import {Link, useLocation} from "react-router-dom";
 
 function Nav(props) {
+    let location = useLocation();
+
     return (
 
         <nav className={"bg-white px-2 sm:px-0  py-3  border-b-2 border-gray-300"}>
             <PageContainer>
                 <div className={"grid grid-cols-4"}>
-                    <a className={"text-green-500 col-start-1 col-end-3 font-extrabold tracking-tight text-2xl  md:text-3xl  "}
-                       href="#">F<span className={"text-black"}>oo</span>dies<span className={"text-black"}>.</span></a>
+                    <Link to={"/Home"} className={"text-green-500 col-start-1 col-end-3 font-extrabold tracking-tight text-2xl  md:text-3xl  "}
+                       >F<span className={"text-black"}>oo</span>dies<span className={"text-black"}>.</span></Link>
 
 
                     <ul className={"text-gray-600 col-start-4 hidden grid-cols-3 gap-3  justify-center m-auto items-center md:grid font-bold"}>
-                        <li className={"hover:text-green-500"}><a href="">Home</a></li>
-                        <li className={"hover:text-green-500"}><a href="">About</a></li>
-                        <li className={"hover:text-green-500"}><a href="">Services</a></li>
+                        <li className={`${location.pathname == "/Home"? "text-green-600 text-lg pointer-events-none": "hover:text-green-500"}`}><a href="">Home</a></li>
+                        <li className={`${location.pathname == "/About"? "text-green-600 text-lg pointer-events-none": "hover:text-green-500"}`}><a href="">About</a></li>
+                        <li className={`${location.pathname == "/Services"? "text-green-600 text-lg pointer-events-none": "hover:text-green-500"}`}><a href="">Services</a></li>
                     </ul>
 
                     <div
