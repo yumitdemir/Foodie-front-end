1fdab82725fa0cba324b4d8877320c7c5ac28c39
Yumit Demir <yumitdemir@outlook.com>
2023-06-08 10:40:49 +0200

    Add functionality to search button.

--- src/pages/Home/Components/SearchInputField.jsx
+++ src/pages/Home/Components/SearchInputField.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {BiSearchAlt} from "react-icons/bi";
+import {Link} from "react-router-dom";
 
-function SearchInputField({searchDispatch,searchState}) {
+function SearchInputField({searchDispatch, searchState}) {
 
     return (
         <div className={"flex flex-row shadow-lg shadow-green-100 rounded-3xl border-2  border-green-600"}>
@@ -14,9 +15,18 @@ function SearchInputField({searchDispatch,searchState}) {
                 }}
                 value={searchState.Input}
             />
-            <button
-                className={"rounded-e-2xl bg-green-600 flex items-center  px-8 font-bold text-white hover:bg-green-700"}>
-                <BiSearchAlt className={"text-3xl"}/></button>
+            {searchState.Input == "" ?
+                <a
+                    className={"rounded-e-2xl bg-green-600 flex items-center  px-8 font-bold text-white hover:bg-green-700"}>
+                    <BiSearchAlt className={"text-3xl"}/>
+                </a>
+                :
+                <Link
+                    to={`/${searchState.IgnorePantry}/${searchState.OrderBy}/${searchState.Input}`}
+                    className={"rounded-e-2xl bg-green-600 flex items-center  px-8 font-bold text-white hover:bg-green-700"}>
+                    <BiSearchAlt className={"text-3xl"}/>
+                </Link>}
+
         </div>
     );
 }
