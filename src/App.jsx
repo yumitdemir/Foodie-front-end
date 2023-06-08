df187ddb59d9390da0accceac6526c2ee706f378
Yumit Demir <yumitdemir@outlook.com>
2023-06-08 10:19:13 +0200

    Add react-router-dom package. Create the home router.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Nav from "./components/nav/nav.jsx";
 import Index from "./pages/Home/Index.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
 
 function App(props) {
+    const router = createBrowserRouter([
+        {path: "/", element: <Navigate to="/Home" replace/>},
+        {path: "/Home", element: <Index/> },
+    ])
     return (
         <>
                 <Nav/>
-                <Index/>
+                <RouterProvider router={router}/>
                 <Footer/>
         </>
     );
