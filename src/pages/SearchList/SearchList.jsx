db76d94b88b86b6ed24b9d9f96fe892ffe24cf82
Yumit Demir <yumitdemir@outlook.com>
2023-06-08 13:31:08 +0200

    Write the fetch function for the react query.

--- src/pages/SearchList/SearchList.jsx
+++ src/pages/SearchList/SearchList.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import {useParams} from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
 
-const fetchRecipes = (pantry, order, input) => {
-    const apiKey = '49a32b520f314d1294660ea61e7ff18e';
-    const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${""}&number=${""}&apiKey=${apiKey}`;
 
-    fetch(url)
-        .then(response => response.json())
-        .catch(error => console.error(error));
-};
+async function fetchRecipes(pantry, order, input) {
+    const apiKey = '49a32b520f314d1294660ea61e7ff18e';
+    input = input.split(" ").join(",+");
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${input}&number=${5}&apiKey=${apiKey}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Failed to fetch recipes');
+    }
+    return response.json();
+}
 
 function SearchList(props) {
     const {pantry, order, input} = useParams();
@@ -18,7 +21,7 @@ function SearchList(props) {
         [`recipies`],
         () => fetchRecipes(pantry, order, input)
     )
-
+    console.log(recipes.data)
     return (
         <div>{pantry + order + input}
 
