21ee6aaf1393c542c8f9bde32e827c3b4e939321
Yumit Demir <yumitdemir@outlook.com>
2023-06-12 18:53:44 +0200

    Fix the bug that causes height and width to be reverse when entered

--- src/components/loadingspinner/LoadingSpinner.jsx
+++ src/components/loadingspinner/LoadingSpinner.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function LoadingSpinner({width,height,boldness}) {
     return (
         <div
-            className={`inline-block justify-center items-center h-${height} w-${width} animate-spin rounded-full border-${boldness} text-green-600  border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]`}
+            className={`inline-block justify-center items-center h-${width} w-${height} animate-spin rounded-full border-${boldness} text-green-600  border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]`}
             role="status">
-            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+
         </div>
     );
 }
