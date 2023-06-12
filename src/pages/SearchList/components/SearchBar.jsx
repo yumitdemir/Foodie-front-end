b4b331b944170f49c205ab9deb5697002366562d
Yumit Demir <yumitdemir@outlook.com>
2023-06-12 16:25:49 +0200

    Fix search bar visual bug

--- src/pages/SearchList/components/SearchBar.jsx
+++ src/pages/SearchList/components/SearchBar.jsx
@@ -7,18 +7,22 @@ import {IoMdAddCircleOutline} from "react-icons/io";
 function SearchBar({inputArray,order,input,pantry}) {
 
     return (
-        <div className="flex flex-col w-full bg-green-500 rounded-xl py-5 px-4 overflow-hidden gap-3 sticky">
-            <p className={"text-lg"}>Add new ingredients</p>
+        <div className="flex flex-col w-full h-fit bg-green-500 rounded-xl py-5 px-4 overflow-hidden gap-3 sticky top-5">
+            <p className={"text-lg font-thin  text-title-black"}>Add new ingredients</p>
             <div className={"flex gap-1 items-center"}>
                 <input type="text" className={"rounded w-full px-3 py-1"}/>
                 <p className={"text-3xl text-white cursor-pointer hover:text-opacity-80"}><IoMdAddCircleOutline/></p>
             </div>
 
             <PantryInput defaultChecked={`${pantry === "true"? "checked":""}`} className={"checked:accent-green-600"} containerClassName={"text-white"}/>
+
+
+            <p className={"text-lg font-thin  text-title-black"}>Order recipes</p>
+            <OrderRecipes  value={order} className={"p-2 rounded-xl"}/>
+
             <SelectedIngredients inputArray={inputArray}/>
 
-            <p className={"text-lg"}>Order recipes</p>
-            <OrderRecipes  value={order} className={"p-2 rounded-xl"}/>
+            <button className={" rounded-lg py-2 bg-white"}>Search</button>
         </div>
     );
 }
