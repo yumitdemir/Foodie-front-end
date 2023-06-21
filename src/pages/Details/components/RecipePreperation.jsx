e346d9f0f032c08154024d578b6c4ce3e0ccf97a
Yumit Demir <yumitdemir@outlook.com>
2023-06-21 18:19:05 +0200

    Create RecipeSteps.jsx

--- src/pages/Details/components/RecipePreperation.jsx
+++ src/pages/Details/components/RecipePreperation.jsx
@@ -1,34 +1,13 @@
 import React from 'react';
 import RecipeIngredients from "./RecipeIngredients.jsx";
 import DetailsTitle from "./ui/DetailsTitle.jsx";
+import RecipeSteps from "./RecipeSteps.jsx";
 
 function RecipePreperation(props) {
     return (
         <div>
             <RecipeIngredients/>
-
-            <DetailsTitle title={"Steps"}/>
-            <div>
-                <p className={"text-lg font-bold "}>Step 1</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
-            </div>
-            <div>
-                <p className={"text-lg font-bold "}>Step 1</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
-            </div> <div>
-            <p className={"text-lg font-bold "}>Step 1</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
-        </div>
-            <div>
-                <p className={"text-lg font-bold "}>Step 1</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
-            </div>
-            <div>
-                <p className={"text-lg font-bold "}>Step 1</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
-            </div>
-
-
+            <RecipeSteps/>
         </div>
     );
 }
---
+++ src/pages/Details/components/RecipeSteps.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import DetailsTitle from "./ui/DetailsTitle.jsx";
+
+function RecipeSteps(props) {
+    return (
+        <div>
+            <DetailsTitle title={"Steps"}/>
+            <div>
+                <p className={"text-lg font-bold "}>Step 1</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
+            </div>
+            <div>
+                <p className={"text-lg font-bold "}>Step 1</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
+            </div> <div>
+            <p className={"text-lg font-bold "}>Step 1</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
+        </div>
+            <div>
+                <p className={"text-lg font-bold "}>Step 1</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
+            </div>
+            <div>
+                <p className={"text-lg font-bold "}>Step 1</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet in ipsam minima tempora temporibus ut. Nisi non provident quidem voluptatibus!</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default RecipeSteps;
